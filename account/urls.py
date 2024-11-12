577ebf84369426a2b65a7ba24953637b44209d43
Arif <arif.onedirection0011@gmail.com>
2024-11-12 11:29:09 +0600

    added csrf_exempt to graphql url

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 from account.schema import *
 
 urlpatterns = [
-    path("graphql/", GraphQLView.as_view(graphiql=True, schema=designation_schema)),
+    path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=designation_schema))),
 ]
