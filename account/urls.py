ec01747e79b32fe663cf36a53bb69022bfe85ef9
Arif <arif.onedirection0011@gmail.com>
2024-11-12 17:15:08 +0600

    Added designation create, update and delete query

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
-from account.schema import *
+from query_matrix.schema import *
 
 urlpatterns = [
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
